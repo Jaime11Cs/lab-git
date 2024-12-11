5acccac365ecebd286854dc093eba9ba0a0d2e88
Jaime11Cs <jaimecarretosanchez@gmail.com>
2024-12-11 11:46:15 +0100

    Modificando el script de envío a SLURM y ajustando file-cut.sh

--- file-cut.sh
+++ file-cut.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-#SBATCH -p hpc-bio-pacioli
-#SBATCH --chdir=/home/alumno03/practica3/lab-git
-#SBATCH -J submit_lab_pacioli-1-alumno03
-#SBATCH --cpus-per-task=4
-#SBATCH --output=/home/alumno03/practica3/lab-git/slurm-j%.out
+
 
 for i in *.fastq
 do
--- submit-lab4_pacioli-alumno03.sh
+++ submit-lab4_pacioli-alumno03.sh
@@ -3,14 +3,9 @@
 #SBATCH --chdir=/home/alumno03/practica3/lab-git
 #SBATCH -J submit_lab_pacioli-1-alumno03
 #SBATCH --cpus-per-task=4
-#SBATCH --output=/home/alumno03/practica3/lab-git/slurm-j%.out
+#SBATCH --output=/home/alumno03/practica3/lab-git/slurm-%j.out
 
-for i in *.fastq
-do
-  total_lineas=$(wc -l < "$i")
-  tercio=$((total_lineas / 3))
-  head -n "$tercio" "$i" > "${i%.fastq}_cortado.fastq"
-  echo "Archivo procesado: $i -> ${i%.fastq}_cortado.fastq"
-done
+#Llamar al script file-cut.sh
+bash file-cut.sh
 
 
